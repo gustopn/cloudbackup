f249da84b84f808e81493ea1b0b5cb3a78031575
Lars Schotte <gustik@gustik.eu>
2018-08-22 21:13:56 +0200

    added script files

---
+++ backup_dir.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -x
+. ./backup_vars.txt
+if [ -d "$1" ]
+then \
+  currentdir=$1
+else \
+  currentdir=$defaultdir
+fi
+currentdate=$(date +%F)
+currentfile="${tarfileprefix}${currentdate}"
+tarsnapstring="/usr/local/bin/tarsnap \
+  --keyfile $tarsnapkeyfile \
+  --cachedir $tarsnapcachedir"
+
+fix_tarsnap_cachedir() {
+  $tarsnapstring --fsck
+  $tarsnapstring -c -f $currentfile $currentdir
+}
+
+if $tarsnapstring --list-archives | grep "$currentfile" >>/dev/null
+then \
+  echo "file exists already, doing nothing"
+else \
+  $tarsnapstring -c -f $currentfile $currentdir || fix_tarsnap_cachedir
+fi
+
